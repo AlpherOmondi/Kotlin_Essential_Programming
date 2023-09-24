924aef7d0b9cb54e98d320283eef6cb1e4f955b4
AlpherOmondi <alphaomondi001@gmail.com>
2023-09-24 18:22:01 +0300

    Added function that returns unit

--- src/main/kotlin/Scopes/FunctionNameWithTitle.kt
+++ src/main/kotlin/Scopes/FunctionNameWithTitle.kt
@@ -1,9 +1,13 @@
 package Scopes
-
+// 1. Function prints a string with interppolated strings
  fun functionnameWithTitle( name:String, title:String = "Mr."){
      println(" My name is [$title] $name")
-
+ }
+// 2. Function returns unit
+fun helloworld():Unit{
+    print("Hello world")
 }
 fun main(){
     functionnameWithTitle("Alpha Omondi")
+    helloworld()
 }

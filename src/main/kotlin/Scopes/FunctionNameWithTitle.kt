ba0be9be816d5dc7e7db7bfb44420abaaa6e347e
AlpherOmondi <alphaomondi001@gmail.com>
2023-09-24 18:17:54 +0300

    Added a function that prints  a string with variables

---
+++ src/main/kotlin/Scopes/FunctionNameWithTitle.kt
@@ -0,0 +1,9 @@
+package Scopes
+
+ fun functionnameWithTitle( name:String, title:String = "Mr."){
+     println(" My name is [$title] $name")
+
+}
+fun main(){
+    functionnameWithTitle("Alpha Omondi")
+}
